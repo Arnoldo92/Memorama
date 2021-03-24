fab429a5f7b43de4e72f96ba1c4935961787c940
arnoldo <Mauri@LAPTOP-0Q9ADC5L>
2021-03-23 19:26:23 -0700

    Agregar archivos de proyecto.

---
+++ Memorama/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace Memorama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MEMORAMA = new System.Windows.Forms.Label();
+            this.CHIVAS = new System.Windows.Forms.PictureBox();
+            this.CHIVAS1 = new System.Windows.Forms.PictureBox();
+            this.DEFENSA = new System.Windows.Forms.PictureBox();
+            this.DEFENSA1 = new System.Windows.Forms.PictureBox();
+            this.GOL = new System.Windows.Forms.PictureBox();
+            this.GOL1 = new System.Windows.Forms.PictureBox();
+            this.ESCUDO = new System.Windows.Forms.PictureBox();
+            this.ESCUDO1 = new System.Windows.Forms.PictureBox();
+            this.FESTEJO = new System.Windows.Forms.PictureBox();
+            this.FESTEJO1 = new System.Windows.Forms.PictureBox();
+            this.NORMA = new System.Windows.Forms.PictureBox();
+            this.NORMA1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox13 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CHIVAS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CHIVAS1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DEFENSA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DEFENSA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GOL)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GOL1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ESCUDO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ESCUDO1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FESTEJO)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FESTEJO1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NORMA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NORMA1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MEMORAMA
+            // 
+            this.MEMORAMA.AutoSize = true;
+            this.MEMORAMA.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.MEMORAMA.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.MEMORAMA.Location = new System.Drawing.Point(552, 93);
+            this.MEMORAMA.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.MEMORAMA.Name = "MEMORAMA";
+            this.MEMORAMA.Size = new System.Drawing.Size(71, 13);
+            this.MEMORAMA.TabIndex = 0;
+            this.MEMORAMA.Text = "MEMORAMA";
+            this.MEMORAMA.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // CHIVAS
+            // 
+            this.CHIVAS.Image = ((System.Drawing.Image)(resources.GetObject("CHIVAS.Image")));
+            this.CHIVAS.Location = new System.Drawing.Point(52, 172);
+            this.CHIVAS.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CHIVAS.Name = "CHIVAS";
+            this.CHIVAS.Size = new System.Drawing.Size(150, 69);
+            this.CHIVAS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CHIVAS.TabIndex = 1;
+            this.CHIVAS.TabStop = false;
+            // 
+            // CHIVAS1
+            // 
+            this.CHIVAS1.Image = ((System.Drawing.Image)(resources.GetObject("CHIVAS1.Image")));
+            this.CHIVAS1.Location = new System.Drawing.Point(250, 172);
+            this.CHIVAS1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.CHIVAS1.Name = "CHIVAS1";
+            this.CHIVAS1.Size = new System.Drawing.Size(150, 69);
+            this.CHIVAS1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CHIVAS1.TabIndex = 2;
+            this.CHIVAS1.TabStop = false;
+            // 
+            // DEFENSA
+            // 
+            this.DEFENSA.Image = ((System.Drawing.Image)(resources.GetObject("DEFENSA.Image")));
+            this.DEFENSA.Location = new System.Drawing.Point(464, 172);
+            this.DEFENSA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DEFENSA.Name = "DEFENSA";
+            this.DEFENSA.Size = new System.Drawing.Size(150, 69);
+            this.DEFENSA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DEFENSA.TabIndex = 3;
+            this.DEFENSA.TabStop = false;
+            // 
+            // DEFENSA1
+            // 
+            this.DEFENSA1.Image = ((System.Drawing.Image)(resources.GetObject("DEFENSA1.Image")));
+            this.DEFENSA1.Location = new System.Drawing.Point(650, 172);
+            this.DEFENSA1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DEFENSA1.Name = "DEFENSA1";
+            this.DEFENSA1.Size = new System.Drawing.Size(150, 69);
+            this.DEFENSA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.DEFENSA1.TabIndex = 4;
+            this.DEFENSA1.TabStop = false;
+            // 
+            // GOL
+            // 
+            this.GOL.Image = ((System.Drawing.Image)(resources.GetObject("GOL.Image")));
+            this.GOL.Location = new System.Drawing.Point(52, 274);
+            this.GOL.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GOL.Name = "GOL";
+            this.GOL.Size = new System.Drawing.Size(150, 69);
+            this.GOL.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GOL.TabIndex = 5;
+            this.GOL.TabStop = false;
+            // 
+            // GOL1
+            // 
+            this.GOL1.Image = ((System.Drawing.Image)(resources.GetObject("GOL1.Image")));
+            this.GOL1.Location = new System.Drawing.Point(250, 274);
+            this.GOL1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.GOL1.Name = "GOL1";
+            this.GOL1.Size = new System.Drawing.Size(150, 69);
+            this.GOL1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.GOL1.TabIndex = 6;
+            this.GOL1.TabStop = false;
+            // 
+            // ESCUDO
+            // 
+            this.ESCUDO.Image = ((System.Drawing.Image)(resources.GetObject("ESCUDO.Image")));
+            this.ESCUDO.Location = new System.Drawing.Point(464, 274);
+            this.ESCUDO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ESCUDO.Name = "ESCUDO";
+            this.ESCUDO.Size = new System.Drawing.Size(150, 69);
+            this.ESCUDO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ESCUDO.TabIndex = 7;
+            this.ESCUDO.TabStop = false;
+            // 
+            // ESCUDO1
+            // 
+            this.ESCUDO1.Image = ((System.Drawing.Image)(resources.GetObject("ESCUDO1.Image")));
+            this.ESCUDO1.Location = new System.Drawing.Point(650, 274);
+            this.ESCUDO1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ESCUDO1.Name = "ESCUDO1";
+            this.ESCUDO1.Size = new System.Drawing.Size(150, 69);
+            this.ESCUDO1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ESCUDO1.TabIndex = 8;
+            this.ESCUDO1.TabStop = false;
+            // 
+            // FESTEJO
+            // 
+            this.FESTEJO.Image = ((System.Drawing.Image)(resources.GetObject("FESTEJO.Image")));
+            this.FESTEJO.Location = new System.Drawing.Point(52, 368);
+            this.FESTEJO.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FESTEJO.Name = "FESTEJO";
+            this.FESTEJO.Size = new System.Drawing.Size(150, 69);
+            this.FESTEJO.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FESTEJO.TabIndex = 9;
+            this.FESTEJO.TabStop = false;
+            // 
+            // FESTEJO1
+            // 
+            this.FESTEJO1.Image = ((System.Drawing.Image)(resources.GetObject("FESTEJO1.Image")));
+            this.FESTEJO1.Location = new System.Drawing.Point(250, 368);
+            this.FESTEJO1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.FESTEJO1.Name = "FESTEJO1";
+            this.FESTEJO1.Size = new System.Drawing.Size(150, 69);
+            this.FESTEJO1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.FESTEJO1.TabIndex = 10;
+            this.FESTEJO1.TabStop = false;
+            // 
+            // NORMA
+            // 
+            this.NORMA.Image = ((System.Drawing.Image)(resources.GetObject("NORMA.Image")));
+            this.NORMA.Location = new System.Drawing.Point(450, 368);
+            this.NORMA.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NORMA.Name = "NORMA";
+            this.NORMA.Size = new System.Drawing.Size(150, 69);
+            this.NORMA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.NORMA.TabIndex = 11;
+            this.NORMA.TabStop = false;
+            // 
+            // NORMA1
+            // 
+            this.NORMA1.Image = ((System.Drawing.Image)(resources.GetObject("NORMA1.Image")));
+            this.NORMA1.Location = new System.Drawing.Point(650, 368);
+            this.NORMA1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.NORMA1.Name = "NORMA1";
+            this.NORMA1.Size = new System.Drawing.Size(150, 69);
+            this.NORMA1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.NORMA1.TabIndex = 12;
+            this.NORMA1.TabStop = false;
+            this.NORMA1.Click += new System.EventHandler(this.NORMA1_Click);
+            // 
+            // pictureBox13
+            // 
+            this.pictureBox13.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox13.Image")));
+            this.pictureBox13.Location = new System.Drawing.Point(939, 172);
+            this.pictureBox13.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox13.Name = "pictureBox13";
+            this.pictureBox13.Size = new System.Drawing.Size(150, 69);
+            this.pictureBox13.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox13.TabIndex = 13;
+            this.pictureBox13.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(1107, 537);
+            this.Controls.Add(this.pictureBox13);
+            this.Controls.Add(this.NORMA1);
+            this.Controls.Add(this.NORMA);
+            this.Controls.Add(this.FESTEJO1);
+            this.Controls.Add(this.FESTEJO);
+            this.Controls.Add(this.ESCUDO1);
+            this.Controls.Add(this.ESCUDO);
+            this.Controls.Add(this.GOL1);
+            this.Controls.Add(this.GOL);
+            this.Controls.Add(this.DEFENSA1);
+            this.Controls.Add(this.DEFENSA);
+            this.Controls.Add(this.CHIVAS1);
+            this.Controls.Add(this.CHIVAS);
+            this.Controls.Add(this.MEMORAMA);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.CHIVAS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CHIVAS1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DEFENSA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DEFENSA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GOL)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GOL1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ESCUDO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ESCUDO1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FESTEJO)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FESTEJO1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NORMA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NORMA1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox13)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MEMORAMA;
+        private System.Windows.Forms.PictureBox CHIVAS;
+        private System.Windows.Forms.PictureBox CHIVAS1;
+        private System.Windows.Forms.PictureBox DEFENSA;
+        private System.Windows.Forms.PictureBox DEFENSA1;
+        private System.Windows.Forms.PictureBox GOL;
+        private System.Windows.Forms.PictureBox GOL1;
+        private System.Windows.Forms.PictureBox ESCUDO;
+        private System.Windows.Forms.PictureBox ESCUDO1;
+        private System.Windows.Forms.PictureBox FESTEJO;
+        private System.Windows.Forms.PictureBox FESTEJO1;
+        private System.Windows.Forms.PictureBox NORMA;
+        private System.Windows.Forms.PictureBox NORMA1;
+        private System.Windows.Forms.PictureBox pictureBox13;
+    }
+}
+
